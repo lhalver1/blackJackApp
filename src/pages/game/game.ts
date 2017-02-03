6d85c4398b385ee34858d065152acbc71e122d88
Logan Halverson <logan.halverson@clareitysecurity.com>
2017-02-03 10:30:49 -0700

    images and view improvement

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { estimateHeight } from 'ionic-angular/es2015/components/virtual-scroll/virtual-util';
+import { Component, trigger, state, style, transition, animate, group } from '@angular/core';
 
 import { NavController, NavParams } from 'ionic-angular';
 
@@ -7,70 +8,88 @@ import { Card } from '../../models/card';
 import { Deck } from '../../models/deck';
 import { Settings } from '../../models/settings';
 
+declare let $: any;          //Jquery
 
 @Component({
-  selector: 'game-page',
-  templateUrl: 'game.html'
+    selector: 'game-page',
+    templateUrl: 'game.html',
+    animations: [
+        trigger('winnerState', [
+            state('hide', style({
+                visibility: 'hidden',
+                height: 0
+            })),
+            state('show', style({
+                visibility: 'visible',
+                height: '*'
+            })),
+            transition('hide => show', animate('400ms ease-in')),
+            transition('show => hide', animate('400ms ease-out'))
+        ])
+    ]
 })
 export class GamePage {
-  playerTurnIndex: number;
-  players: Player[];
-  winningPlayers: Player[];
-  deck: Deck;
-  trash: Card[];
-  settings: Settings;
+    showWinners: string;
+    playerTurnIndex: number;
+    players: Player[];
+    winningPlayers: Player[];
+    deck: Deck;
+    trash: Card[];
+    settings: Settings;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // Get the players
-    this.players = [
-      new Player("Dealer", [], false, "CPU", 0, 0),
-      new Player("Logan", [], false, "Human", 0, 0)
-    ];
-    this.winningPlayers = [];
-    this.trash = [];
-    this.settings = new Settings(2000, false, false);
+    constructor(public navCtrl: NavController, public navParams: NavParams) {
+        this.showWinners = 'hide';
+        // Get the players
+        this.players = [
+            new Player("Dealer", [], false, "CPU", 0, 0),
+            new Player("Jalen", [], false, "CPU", 0, 0),
+            new Player("Logan", [], false, "Human", 0, 0)
+        ];
+        this.winningPlayers = [];
+        this.trash = [];
+        this.settings = new Settings(2000, false, false);
 
-    // Get the deck of cards
-    this.deck = new Deck();
-    this.deck.buildDeck()
-    this.deck.shuffle();
-    
-    this.dealOutCards();
-  }
+        // Get the deck of cards
+        this.deck = new Deck();
+        this.deck.buildDeck()
+        this.deck.shuffle();
 
-  ngAfterViewInit() {
-  }
+        this.dealOutCards();
+    }
 
-  /**
-   * Deals out cards to the player and the dealer. First makes sure the
-   * gaming flag is set and that both hands are empty, if not then it 
-   * puts the cards in the hands in the trash pile.
-   */
-  dealOutCards() {
-      // Take each players cards and push them to the trash and then
-      // clear the players hand.
-      for (var i = 0; i < this.players.length; i++) {
-          var currPlayer = this.players[i];
-          if (currPlayer.hand.length > 0) {
-              for (var index = 0; index < currPlayer.hand.length; index++) {
-                  var currCard = currPlayer.hand[index];
-                  this.trash.push(currCard);
-              }
-              currPlayer.hand.splice(0, currPlayer.hand.length);// = [];
-          }
-      }
+    ngAfterViewInit() {
+    }
 
-      // If there are 12 or less cards remaining do a reshuffle.
-      if (this.deck.cards.length <= 12) {
-          this.deck.buildDeck();
-      }
+    /**
+     * Deals out cards to the player and the dealer. First makes sure the
+     * gaming flag is set and that both hands are empty, if not then it 
+     * puts the cards in the hands in the trash pile.
+     */
+    dealOutCards() {
+        // Take each players cards and push them to the trash and then
+        // clear the players hand.
+        for (var i = 0; i < this.players.length; i++) {
+            var currPlayer = this.players[i];
+            if (currPlayer.hand.length > 0) {
+                for (var index = 0; index < currPlayer.hand.length; index++) {
+                    var currCard = currPlayer.hand[index];
+                    this.trash.push(currCard);
+                }
+                currPlayer.hand.splice(0, currPlayer.hand.length);// = [];
+            }
+        }
+
+        // If there are 12 or less cards remaining do a reshuffle.
+        if (this.deck.cards.length <= 12) {
+            this.deck.buildDeck();
+        }
 
         // Pass out cards to each player, each player gets 1 card
         // then each player gets their 2nd card.
-        for(var x = 0; x < 2 ; x++) {
+        for (var x = 0; x < 2; x++) {
             for (var i = 0; i < this.players.length; i++) {
                 var currPlayer = this.players[i];
-                var currCard = this.deck.cards.splice(0,1)[0];
+                var currCard = this.deck.cards.splice(0, 1)[0];
                 currPlayer.hand.push(currCard);
             }
         }
@@ -80,25 +99,25 @@ export class GamePage {
         // the hand.
         this.resetPlayerTurns();
         this.players[this.playerTurnIndex].turn = true;
-  }
+    }
 
     /**
      * The next player is a Bot or Dealer, be smart for them.
      */
     goBeABot(player: Player) {
-      debugger;
+        // debugger;
         var total = player.cardsTotal();
         var bustedPlayers = [];
         var highestVisibleTotal = -1;
         var thisPlayersRealLifeIndex = -1;
-    
+
         for (var index = 0; index < this.players.length; index++) {
             let currPlayer: Player = this.players[index];
-    
+
             if (currPlayer != player) {
                 var playersTotal = currPlayer.cardsTotal();
                 if (currPlayer.turn || playersTotal === 21) {
-    
+
                     if (playersTotal > 21) {
                         bustedPlayers.push(currPlayer);
                     } else if (playersTotal > highestVisibleTotal) {
@@ -106,104 +125,103 @@ export class GamePage {
                     }
                 }
             } else {
-                thisPlayersRealLifeIndex = index+1;
+                thisPlayersRealLifeIndex = index + 1;
             }
-    
+
         }
 
         let playersTogoYet = 0;
-        for (var index = this.players.length-1; index < 0; index--) {
+        for (var index = 0; index < this.players.length; index++) {
             let currPlayer: Player = this.players[index];
-            if (currPlayer === player ) {
+            if (currPlayer === player) {
                 break;
             } else {
                 playersTogoYet += 1;
             }
         }
-    
+
         // var remainingPlayersThatHaventBusted = ($scope.players.length-1) - bustedPlayers.length;
-        let thisPlayersArrayIndex = thisPlayersRealLifeIndex-1;
+        let thisPlayersArrayIndex = thisPlayersRealLifeIndex - 1;
 
         // var playersInfrontOfMeThatHaventBusted = thisPlayersArrayIndex - bustedPlayers.length;
         var numPlayersBehindMe = this.players.length - thisPlayersArrayIndex;
         if (total === 21) {
             setTimeout(() => {
-                this.stay();
+                this.stay(player);
             }, this.settings.cpuDecisionTime);
-        } else if(total <= 16 && playersTogoYet > 0 && highestVisibleTotal > total) {
+        } else if (total <= 16 && playersTogoYet > 0 && highestVisibleTotal > total) {
             setTimeout(() => {
-                this.hit();
+                this.hit(player);
             }, this.settings.cpuDecisionTime);
-        } else if(total <= 16 && playersTogoYet === 0 && highestVisibleTotal <= total) {
+        } else if (total <= 16 && playersTogoYet === 0 && highestVisibleTotal <= total) {
             setTimeout(() => {
-                this.stay();
+                this.stay(player);
             }, this.settings.cpuDecisionTime);
-        } else if(total > 16 && playersTogoYet > 0 && highestVisibleTotal <= total) {
+        } else if (total > 16 && playersTogoYet > 0 && highestVisibleTotal <= total) {
             setTimeout(() => {
-                this.stay();
+                this.stay(player);
             }, this.settings.cpuDecisionTime);
-        } else if(total > 16 && playersTogoYet === 0 && highestVisibleTotal > total) {
+        } else if (total > 16 && playersTogoYet === 0 && highestVisibleTotal > total) {
             setTimeout(() => {
-                this.hit();
+                this.hit(player);
             }, this.settings.cpuDecisionTime);
-        } else if(total <= 16 && playersTogoYet > 0 && highestVisibleTotal <= total) {
+        } else if (total <= 16 && playersTogoYet > 0 && highestVisibleTotal <= total) {
             setTimeout(() => {
-                this.hit();
+                this.hit(player);
             }, this.settings.cpuDecisionTime);
-        } else if(total <= 16 && playersTogoYet === 0 && highestVisibleTotal > total) {
+        } else if (total <= 16 && playersTogoYet === 0 && highestVisibleTotal > total) {
             setTimeout(() => {
-                this.hit();
+                this.hit(player);
             }, this.settings.cpuDecisionTime);
-        } else if(total > 16 && playersTogoYet > 0 && highestVisibleTotal > total) {
+        } else if (total > 16 && playersTogoYet > 0 && highestVisibleTotal > total) {
             setTimeout(() => {
-                this.hit();
+                this.hit(player);
             }, this.settings.cpuDecisionTime);
-        } else if(total > 16 && playersTogoYet === 0 && highestVisibleTotal <= total) {
+        } else if (total > 16 && playersTogoYet === 0 && highestVisibleTotal <= total) {
             setTimeout(() => {
-                this.stay();
+                this.stay(player);
             }, this.settings.cpuDecisionTime);
         }
     }
 
-  /**
-   * Adds a card to the players hand, if the players hand excedes
-   * 21 then the player ends their turn and loses and the hand.
-   */
-  hit() {
-      debugger;
-      let player: Player = this.players[this.playerTurnIndex];
-      player.hand.push(this.deck.cards.splice(0, 1)[0]);
+    /**
+     * Adds a card to the players hand, if the players hand excedes
+     * 21 then the player ends their turn and loses and the hand.
+     */
+    hit(player: Player) {
+        // debugger;
+        player.hand.push(this.deck.cards.splice(0, 1)[0]);
 
-      if (player.type === "Human") {
-          if (player.cardsTotal() >= 22) {
-              this.playerTurnIndex -= 1;
+        if (player.type === "Human") {
+            if (player.cardsTotal() >= 22) {
+                this.playerTurnIndex -= 1;
 
-              //If the next players turn is out of index for players array
-              //Reset it back to the beginning
-              if (this.playerTurnIndex < 0) {
+                //If the next players turn is out of index for players array
+                //Reset it back to the beginning
+                if (this.playerTurnIndex < 0) {
 
-                  this.endRound();
+                    this.endRound();
 
-              } else {
-                  var nextPlayer = this.players[this.playerTurnIndex];
-                  nextPlayer.turn = true;
-                  if (nextPlayer.type == "CPU") {
-                      this.goBeABot(nextPlayer);
-                  }
-              }
-          }
-      } else {
-          this.goBeABot(player);
-      }
+                } else {
+                    var nextPlayer = this.players[this.playerTurnIndex];
+                    nextPlayer.turn = true;
+                    if (nextPlayer.type == "CPU") {
+                        this.goBeABot(nextPlayer);
+                    }
+                }
+            }
+        } else {
+            this.goBeABot(player);
+        }
 
-  }
+    }
 
     /**
      * The player has elected to stay with their hand. End
      * their turn.
      */
-    stay() {
-      debugger;
+    stay(player: Player) {
+        // debugger;
         this.playerTurnIndex -= 1;
 
         //If the next players turn is out of index for players array
@@ -215,7 +233,7 @@ export class GamePage {
         } else {
             let nextPlayer: Player = this.players[this.playerTurnIndex];
             nextPlayer.turn = true;
-            if(nextPlayer.type == "CPU") {
+            if (nextPlayer.type == "CPU") {
                 this.goBeABot(nextPlayer);
             }
         }
@@ -227,12 +245,12 @@ export class GamePage {
      */
     endRound() {
         this.determineWinner();
-
+        this.showWinners = 'show';
         setTimeout(() => {
-            this.winningPlayers.splice(0, this.winningPlayers.length);
+            this.showWinners = 'hide';
+            this.winningPlayers.splice(0, this.winningPlayers.length);  
             this.dealOutCards();
         }, 4000);
-
     }
 
     /**
@@ -250,7 +268,7 @@ export class GamePage {
                 this.winningPlayers.splice(0, this.winningPlayers.length);
                 this.winningPlayers.push(currPlayer);
                 highestTotal = currPlayersTotal;
-            } else if (currPlayersTotal === highestTotal){
+            } else if (currPlayersTotal === highestTotal) {
                 this.winningPlayers.push(currPlayer);
             }
         }
@@ -272,17 +290,17 @@ export class GamePage {
         }//End players for
     }
 
-  /**
-   * Goes through the players list and sets the turn flag
-   * to false. Meaning that for the next round that player
-   * hasn't had their turn yet.
-   */
-  resetPlayerTurns() {
-      this.playerTurnIndex = this.players.length-1;
-      for (var index = 0; index < this.players.length; index++) {
-          var player = this.players[index];
-          player.turn = false;
-      }
-  }
+    /**
+     * Goes through the players list and sets the turn flag
+     * to false. Meaning that for the next round that player
+     * hasn't had their turn yet.
+     */
+    resetPlayerTurns() {
+        this.playerTurnIndex = this.players.length - 1;
+        for (var index = 0; index < this.players.length; index++) {
+            var player = this.players[index];
+            player.turn = false;
+        }
+    }
 
 }// End GamePage
