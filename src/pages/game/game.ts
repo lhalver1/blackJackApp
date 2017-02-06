623e0cfc1df8de05e4e21428d56a1bea92c689bd
Logan Halverson <logan.halverson@clareitysecurity.com>
2017-02-06 11:11:33 -0700

    fly out added

--- src/pages/game/game.ts
+++ src/pages/game/game.ts
@@ -46,14 +46,14 @@ declare let $: any;          //Jquery
                 style({opacity: 1, transform: 'translateX(-15px)',  offset: 0.3}),
                 style({opacity: 1, transform: 'translateX(0)',     offset: 1.0})
               ]))
-            ])//,
-            // transition('* => void', [
-            //   animate(300, keyframes([
-            //     style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
-            //     style({opacity: 1, transform: 'translateX(-15px)', offset: 0.7}),
-            //     style({opacity: 0, transform: 'translateX(100%)',  offset: 1.0})
-            //   ]))
-            // ])
+            ]),
+            transition('* => void', [
+              animate(500, keyframes([
+                style({opacity: 1, transform: 'translateX(0)',     offset: 0}),
+                style({opacity: 1, transform: 'translateX(15px)', offset: 0.7}),
+                style({opacity: 0, transform: 'translateX(-100%)',  offset: 1.0})
+              ]))
+            ])
         ])
     ]
 })
